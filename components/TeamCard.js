e2f7f02b01972462d2da03f5faeff213d135059c
Mark Hamilton <ham.mark126@gmail.com>
2023-01-23 21:07:47 -0600

    team card is made

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,7 +1,40 @@
+import Link from 'next/link';
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { deleteTeam } from '../api/teams';
 
-export default function TeamCard() {
+export default function TeamCard({ teamObj, onUpdate }) {
+  const deleteThisTeam = () => {
+    if (window.confirm(`Delete ${teamObj.name}?`)) {
+      deleteTeam(teamObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
   return (
-    <div>TeamCard</div>
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={teamObj.image} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{teamObj.name}</Card.Title>
+        <Link href={`/teams/${teamObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/teams/edit/${teamObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisTeam} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+TeamCard.propTypes = {
+  teamObj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
